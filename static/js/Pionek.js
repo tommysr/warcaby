b97beef2ae587daaaf06a263c3495b51c2894aec
tommysr <47206288+tommysr@users.noreply.github.com>
2020-05-18 23:27:45 +0200

    fixed Pionek class

--- static/js/Pionek.js
+++ static/js/Pionek.js
@@ -2,7 +2,7 @@ class Pionek extends THREE.Mesh {
     constructor(color) {
         super();
         this.geometry = new THREE.CylinderGeometry(40, 40, 25, 32);
-        this.createMaterial(kolor)
+        this.createMaterial(color)
     } 
 
     createMaterial(color){
