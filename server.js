a2ba41fb4d58fd8f10dad69096a74c796804b46c
matzayonc <matzayonc@gmail.com>
2020-05-19 10:38:56 +0200

    server

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const http = require("http")
+const path = require("path")
 const qs = require("querystring")
 const fs = require("fs")
 const mime = require("mime-types")
-const path = require("path")
 
 let players = [];
-let pionkiTab = []
+let pawns = []
 
 
 const addPlayer = (name) => {
@@ -27,9 +27,9 @@ const addPlayer = (name) => {
 }
 
 
-const resetBoard = (res) => {
+function resetBoard(){
     players = []
-    pionkiTab = [
+    pawns = [
         [0, 2, 0, 2, 0, 2, 0, 2],
         [2, 0, 2, 0, 2, 0, 2, 0],
         [0, 0, 0, 0, 0, 0, 0, 0],
@@ -42,67 +42,33 @@ const resetBoard = (res) => {
 }
 resetBoard()
 
-const updateTab = (finishObj, res) => {
-    pionkiTab = JSON.parse(finishObj.data)
-    res.end("ok")
-}
 
-const compareTab = (finishObj, res) =>{
+
+function compare (finishObj, res){
     var obj = {}
 
-    if (finishObj.data === JSON.stringify(pionkiTab)) 
+    if (finishObj.data === JSON.stringify(pawns)) 
         obj.zmiany = "false"
     else {
         obj.zmiany = "true"
-        obj.pionkiTab = pionkiTab
+        obj.pionkiTab = pawns
     }
 
     res.end(JSON.stringify(obj))
 }
 
 
-const serverres = (req, res) => {
-    var allData = "";
-    req.on("data", function (data) {
-        allData += data
-    })
-    req.on("end", function (data) {
-        var finishObj = qs.parse(allData)
+function serverres(data, res){
 
-        switch (finishObj.action) {
-            case "add":
-                res.end(addPlayer(finishObj.name))
-                break
-            case "reset":
-                resetBoard(res)
-                res.end("ok")
-                break
-            case "check":
-                res.end(players[1] ? players[1] : "")
-                break
-            case "update":
-                updateTab(finishObj, res)
-                break
-            case "compare":
-                compareTab(finishObj, res)
-                break
-        }
-    })
 }
 
 var server = http.createServer(function (req, res) {
-    var req = req
-    var res = res
-
     switch (req.method) {
         case "GET":
-            let url =
-            req.url == "/" ? "/index.html" : req.url
+            let url = req.url == "/" ? "/index.html" : req.url
     
-            let type = mime.lookup(url);
-
             fs.readFile(path.join('static', decodeURI(url)), function (error, data) {
-                res.writeHead(200, { "Content-Type": `${type};charset=utf-8` })
+                res.writeHead(200, { "Content-Type": `${mime.lookup(url)};charset=utf-8` })
 
                 if(error)
                     console.error('err:', error)
@@ -114,9 +80,34 @@ var server = http.createServer(function (req, res) {
             break;
         case "POST":
             res.writeHead(200, { "content-type": "text/html;charset=utf-8" })
-            serverres(req, res)
-          break;
-      } 
+            var allData = "";
+            req.on("data", function (data) {
+                allData += data
+            })
+            req.on("end", function () {
+                let data = qs.parse(allData)
+                switch (data.action) {
+                    case "add":
+                        res.end(addPlayer(data.name))
+                        break
+                    case "reset":
+                        resetBoard()
+                        res.end("ok")
+                        break
+                    case "check":
+                        res.end(players[1] ? players[1] : "")
+                        break
+                    case "update":
+                        pawns = JSON.parse(data.data)
+                        res.end("ok")
+                        break
+                    case "compare":
+                        compare(data, res)
+                        break
+                }
+            })
+        break;
+    } 
 })
 
 server.listen(3000, function () {
